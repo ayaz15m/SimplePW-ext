5aef59b485ab974a9c2373afa0356ea232aa0819
Ayaz Merchant <ayaz15m@yahoo.com>
2014-08-12 17:42:29 -0500

    more field ids

--- info.js
+++ info.js
@@ -30,5 +30,6 @@ xhr.send();
 console.log("username", "object.username")
 console.log("pw", "object.password")
 // twitter, facebook, yahoo mail, wellsfargo, linkedin, amazon, ebay, pinterest, paypal, tumblr, instagram, netflix salesforce, vimeo
+//bleacherreport, aol, monster, xfinity/comcast, careerbuilder, github
 $("#signin-email, #email, #username, #userid, #session_key-login, #ap_email, #userEmail, #user_email, #login_email, #signup_email, #id_username, #lgnId1, #EmailAddress, #user, #JobSeekerLoginV3_cbsys_login_email, #login_field").val("ayazm");
 $("#signin-password, #pass, #passwd, #password, #session_password-login, #ap_password, #userPassword, #user_password, #login_password, #signup_password, #id_password, #pwdId1, #Password, #JobSeekerLoginV3_cbsys_login_password").val("object.password");
